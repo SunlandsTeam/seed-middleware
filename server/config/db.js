7955bac24c5c8aac6096d858687223e1feac7657
Donny <1021001390@qq.com>
2018-07-21 10:08:55 +0800

    set operatorsAliases to false

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,7 @@ import '../../env'
 import Sequelize from 'sequelize'
 
 const Todolist = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL || 'localhost'}/todolist`, {
+  operatorsAliases: false,
   define: {
     timestamps: false // 取消Sequelzie自动给数据表加入时间戳（createdAt以及updatedAt）
   }
