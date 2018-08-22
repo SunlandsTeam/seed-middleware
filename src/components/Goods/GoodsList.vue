ce35ce1373e9c401e62f5ded229b8a09bac92803
Donny <1021001390@qq.com>
2018-08-22 14:35:05 +0800

    add moment

--- src/components/Goods/GoodsList.vue
+++ src/components/Goods/GoodsList.vue
@@ -5,11 +5,23 @@
       <el-table-column prop="name" label="名称" width="200" align="center"></el-table-column>
       <el-table-column prop="price" label="价格" align="center"></el-table-column>
       <el-table-column prop="description" label="描述" align="center"></el-table-column>
+      <el-table-column prop="created_at" label="创建时间" align="center">
+        <template slot-scope="scope">
+          {{dateFormat(scope.row.created_at)}}
+        </template>
+      </el-table-column>
+      <el-table-column prop="updated_at" label="修改时间" align="center">
+        <template slot-scope="scope">
+          {{dateFormat(scope.row.updated_at)}}
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
+import * as moment from 'moment'
+
 export default {
   name: 'GoodsList',
   data () {
@@ -24,6 +36,11 @@ export default {
         this.goodsList = res.data.result
       }
     })
+  },
+  methods: {
+    dateFormat (datetime) {
+      return moment(datetime).format('YYYY/MM/DD HH:mm:ss')
+    }
   }
 }
 </script>
